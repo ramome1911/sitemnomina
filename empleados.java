95fec2f8145910664a58a2a81822fe38e271e3cd
Rafael Morales <rmoralesm2@unicartagena.edu.co>
2025-10-03 15:03:14 -0500

    creacion de los metodos base para el calculo de salario, beneficio y mostrar informacion

--- empleados.java
+++ empleados.java
@@ -22,4 +22,12 @@ public abstract class empleados {
     public void setSalario(double salariobase) {
         this.salariobase = salariobase;
     }
+
+    public abstract String mostrarInformacion();
+
+    public abstract double calcularSalariobruto();
+
+    public double calcularBeneficios() {
+        return 0; // Por defecto, sin beneficios adicionales
+    }
 }
