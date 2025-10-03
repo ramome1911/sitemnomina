e7fe1896960dc0e513e07fa0a42ef0b599a45561
valeri castellon <castellonlambisvaleri@gmail.com>
2025-10-03 15:19:04 -0500

    creacion del metodo para  definir las deducciones de los empleados

--- empleados.java
+++ empleados.java
@@ -30,4 +30,9 @@ public abstract class empleados {
     public double calcularBeneficios() {
         return 0; // Por defecto, sin beneficios adicionales
     }
+
+    public double calcularDeducciones() {
+        double deduccion = calcularSalariobruto() * (0.004 + 0.005); // deducciones de Arl,salud y pension
+        return deduccion;
+    }
 }
