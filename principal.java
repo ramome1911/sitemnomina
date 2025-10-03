13efc5180c9cff6ff8c3771fc60fc1df89049e37
Rafael Morales <rmoralesm2@unicartagena.edu.co>
2025-10-03 16:29:16 -0500

    prueba para los empleados por hora

--- principal.java
+++ principal.java
@@ -1,5 +1,7 @@
 public class principal {
     public static void main(String[] args) {
+        EmpleadoHora empleado2 = new EmpleadoHora("María", 17, 20000, false, 0);
+        System.out.println(empleado2.mostrarInformacion());
 
     }
 }
