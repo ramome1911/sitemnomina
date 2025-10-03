2ce0d8db76c5a1931c65daf01a5382d6b3100fdf
AliNaDt <adoriat2@unicartagena.edu.co>
2025-10-03 15:27:34 -0500

    Definición de los métodos para los empleados salariados

--- empleadoSalariados.java
+++ empleadoSalariados.java
@@ -15,3 +15,28 @@ public class empleadoSalariados extends empleados {
         this.antiguedad = antiguedad;
     }
 }
+@Override
+    public double calcularSalariobruto() {
+        double bonoAntiguedad = 0;
+        if (antiguedad > 5) {
+            bonoAntiguedad = salariobase * 0.10; // 10% de bono por antiguedad mayor a 5 años
+            return salariobase + bonoAntiguedad;
+        } else {
+            return salariobase;
+        }
+    }
+    @Override
+    public double calcularBeneficios() {
+        double Alimentacion = 1000000; // bono de alimentacion
+        return Alimentacion;
+    }
+
+    @Override
+    public String mostrarInformacion() {
+        return "Empleado Asalariado: " + "\n" + " nombre: " + getNombre() + "\n"
+                + " Salario Base: " + salariobase + "\n"
+                + " Antiguedad: " + antiguedad + " años" + "\n"
+                + " Beneficios: " + calcularBeneficios() + "\n"
+                + " salario mensual: " + calcularSalariobruto() + "\n"
+                + " deducciones: " + calcularDeducciones();
+    }
