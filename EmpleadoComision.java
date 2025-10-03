2d2fa37509c4c395e64fb22be0c89f0a74ec3420
valeri castellon <castellonlambisvaleri@gmail.com>
2025-10-03 15:57:18 -0500

    definicion de los metodos para los empleados comision

--- EmpleadoComision.java
+++ EmpleadoComision.java
@@ -1,15 +1,44 @@
-public class EmpleadoComision extends empleados { private double ventasMensuales;
+public class EmpleadoComision extends empleados {
+    private double ventasMensuales;
 
-    public EmpleadoComision (String nombre, double salariobase, double ventasMensuales) {
+    public EmpleadoComision(String nombre, double salariobase, double ventasMensuales) {
         super(nombre, salariobase);
         this.ventasMensuales = ventasMensuales;
 
     }
+
     public double getVentasMensuales() {
         return ventasMensuales;
     }
+
     public void setVentasMensuales(double ventasMensuales) {
         this.ventasMensuales = ventasMensuales;
     }
-    
+
+    @Override
+    public double calcularBeneficios() {
+        double Alimentacion = 1000000; // bono de alimentacion
+        return Alimentacion;
+    }
+
+    @Override
+    public double calcularSalariobruto() {
+        if (ventasMensuales > 20000000) {
+            double comision = ventasMensuales * 0.03;
+            return salariobase + comision;
+        } else {
+            return salariobase;
+        }
+
+    }
+
+    @Override
+    public String mostrarInformacion() {
+        return "Empleado por Comisión: " + "\n" + " nombre: " + getNombre() + "\n"
+                + " Salario Base: " + salariobase() + "\n"
+                + " Ventas Mensuales: " + ventasMensuales + "\n"
+                + " Beneficios: " + calcularBeneficios() + "\n"
+                + " salario mensual: " + calcularSalariobruto() + "\n"
+                + " deducciones: " + calcularDeducciones();
+    }
 }
