c1768508640f3661e4b8b2adff0d2bed9439f2dd
Rafael Morales <rmoralesm2@unicartagena.edu.co>
2025-10-03 16:08:43 -0500

    definicion de los metodos para los empleados por hora

--- EmpleadoHora.java
+++ EmpleadoHora.java
@@ -1,10 +1,11 @@
-public class EmpleadoHora extends empleados{
+public class EmpleadoHora extends empleados {
 
     private int horasTrabajadas;
     private boolean fondoAhorro;
     private int añosAfiliacion;
 
-    public EmpleadoHora(String nombre, int horasTrabajadas, double salariobase, boolean fondoAhorro, int añosAfiliacion) {
+    public EmpleadoHora(String nombre, int horasTrabajadas, double salariobase, boolean fondoAhorro,
+            int añosAfiliacion) {
         super(nombre, salariobase);
         this.horasTrabajadas = horasTrabajadas;
         this.fondoAhorro = fondoAhorro;
@@ -33,6 +34,37 @@ public class EmpleadoHora extends empleados{
 
     public void setFondoAhorro(boolean fondoAhorro) {
         this.fondoAhorro = fondoAhorro;
+    }
+
+    @Override
+    public double calcularBeneficios() {
+        double beneficio = 0;
+        if (fondoAhorro && añosAfiliacion > 1) {
+            beneficio = salariobase * 0.02; // 2% fondo ahorro
+        }
+        return beneficio;
+    }
+
+    @Override
+    public double calcularSalariobruto() {
+        if (horasTrabajadas > 40) {
+            double pagoHorasExtra = (horasTrabajadas - 40) * salariobase * 1.5;
+            return (40 * salariobase) + pagoHorasExtra + calcularBeneficios();
+        } else {
+            return horasTrabajadas * salariobase + calcularBeneficios();
+        }
+    }
+
+    @Override
+    public String mostrarInformacion() {
+        return "Empleado por Hora: " + "\n" + " Nombre: " + getnombre() + "\n"
+                + " Horas Trabajadas: " + horasTrabajadas + "\n"
+                + " Salario por Hora: " + salariobase() + "\n"
+                + " Fondo de Ahorro: " + (fondoAhorro ? "Sí" : "No") + "\n"
+                + " Años de Afiliación: " + añosAfiliacion + "\n"
+                + " Beneficios: " + calcularBeneficios() + "\n"
+                + " Deducciones: " + calcularDeducciones() + "\n"
+                + " Salario total: " + calcularSalariobruto();
     }
 
 }
